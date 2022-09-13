8f25730056f497d1cfaebf98a7928d8d1e250e12
Ahmed Baari <ahmed4baari@gmail.com>
2022-09-13 09:29:58 +0530

    Configured for github pages Added Terms of Service, unbeautified Changed sidebar positions for legal content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'The Via Service',
   tagline: 'Documentation, blog, support all in one place',
-  url: 'https://berry.is-a.dev/',
-  baseUrl: '/',
+  url: 'https://ViaService.github.io/',
+  baseUrl: '/Website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'viaservice', // Usually your GitHub org/user name.
-  projectName: 'website', // Usually your repo name.
+  organizationName: 'ViaService', // Usually your GitHub org/user name.
+  projectName: 'Website', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
