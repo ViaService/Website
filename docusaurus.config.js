0cf206f8c533544b6548a6071eb9b462622210eb
Ahmed Baari <ahmed4baari@gmail.com>
2022-09-15 09:29:51 +0530

    algolia setup completion

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,32 @@ const config = {
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
+    ({     algolia: {
+      // The application ID provided by Algolia
+      appId: 'G7Q39IJWC7',
+
+      // Public API key: it is safe to commit it
+      apiKey: '7788b6864c1a6728e8d60a6a675d106b',
+
+      indexName: 'via-docs',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      //... other Algolia params
+      siteId: '427912ba-1525-496d-b3da-e046ad8f02d2',
+      branch: 'master',
+      selector: 'div#search',
+    },
       colorMode: {
         defaultMode: 'dark',
         disableSwitch: false,
